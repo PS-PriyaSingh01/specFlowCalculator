9be892c73fa9e1e758238b780aa6a7d18032cc8b
PS-PriyaSingh01 <99895674+PS-PriyaSingh01@users.noreply.github.com>
2022-11-07 16:13:10 +0530

    Add project files.

---
+++ SpecFlowCalculator.Specs/Steps/CalculatorStepDefinitions.cs
@@ -0,0 +1,53 @@
+﻿using specFlowCalculator;
+using TechTalk.SpecFlow;
+using FluentAssertions;
+using System;
+
+namespace SpecFlowCalculator.Specs.Steps
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+        private readonly ScenarioContext _scenarioContext;
+
+        private readonly Calculator cal = new Calculator();
+
+        private int firstnumber;
+
+        private int secondnumber;
+
+        private int addresult;
+
+        public CalculatorStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            //cal.FirstNumber = number;
+            firstnumber = number;
+        }
+
+        [Given("the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+            // cal.SecondNumber = number;
+            secondnumber = number;
+        }
+
+        [When("the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+           // addresult = cal.Add();
+            addresult = firstnumber + secondnumber;
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            addresult.Should().Be(result);
+        }
+    }
+}
